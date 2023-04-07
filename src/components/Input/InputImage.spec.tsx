377759e402b4b192051ce6513aae7901a34de007
0xb0b1 <vcente82@gmail.com>
2023-04-07 00:43:27 -0400

    test: basic input image component unit testing

--- src/components/Input/InputImage.spec.tsx
+++ src/components/Input/InputImage.spec.tsx
@@ -1,15 +1,55 @@
-import { render, fireEvent } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { ImageProvider } from '../../contexts/ImageContext'
 import { InputImage } from './InputImage'
 
-describe('InputImage', () => {
-  it('should call onChange handler with selected file', () => {
-    const onChange = jest.fn()
-    const { getByTestId } = render(<InputImage handleFileChange={onChange} />)
+let wrapper: any
 
-    const file = new File(['foo'], 'test-image.png', { type: 'image/png' })
-    const input = getByTestId('upload-avatar')
+describe('Image Uploader', () => {
+  beforeEach(() => {
+    wrapper = ({ children }: any): JSX.Element => (
+      <ImageProvider>{children}</ImageProvider>
+    )
+  })
 
-    fireEvent.change(input, { target: { files: [file] } })
-    expect(onChange).toHaveBeenCalledWith(file)
+  it('should render InputImage component correctly', () => {
+    render(<InputImage />)
+
+    const element = screen.getByText('Drop the image here or click to browse.')
+    expect(element).toBeInTheDocument()
+  })
+
+  it('should have an input of type file', () => {
+    render(<InputImage />)
+
+    const uploadAvatarInput: any = screen.getByTestId('upload-avatar')
+    expect(uploadAvatarInput.type).toBe('file')
+  })
+
+  it('should be able to add an image by clicking on the input', async () => {
+    const file = new File(['image'], 'image.png', { type: 'image/png' })
+
+    render(<InputImage />)
+
+    const uploadAvatarInput: any = screen.getByTestId('upload-avatar')
+
+    fireEvent.change(uploadAvatarInput, { target: { files: [file] } })
+
+    expect(uploadAvatarInput.files[0]).toStrictEqual(file)
+  })
+
+  it('should be able to add an image by drag and drop', () => {
+    render(<InputImage />)
+
+    const file = new File(['image'], 'image.png', { type: 'image/png' })
+    const uploadAvatarInput: any = screen.getByTestId('upload-avatar')
+
+    fireEvent.drop(uploadAvatarInput, {
+      target: {
+        files: [file],
+      },
+    })
+
+    expect(uploadAvatarInput.files[0]).toStrictEqual(file)
   })
 })
